f55813eb29719fdb542b886f116b503e0cf6e3a9
Viviane Aparecida Vilas Boas <A0119451@redecorp.br>
2021-04-05 10:11:32 -0300

    projeto final

---
+++ src/data/stocks.js
@@ -0,0 +1,6 @@
+export default [
+    { id: 1, name: 'BMW', price: 142 },
+    { id: 2, name: 'Google', price: 209},
+    { id: 3, name: 'Apple', price: 280 },
+    { id: 4, name: 'Twitter', price: 77},
+]
